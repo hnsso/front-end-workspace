a12de2e8ed037da9e94bb453020759fa2c0e18d8
hnsso <152463185+hnsso@users.noreply.github.com>
2023-12-15 17:49:27 +0900

    hnsso
    asd

--- 3_javaScript/object/js/03_DOM.js
+++ 3_javaScript/object/js/03_DOM.js
@@ -1,20 +1,19 @@
-//  load : 모든 로드가 다 끝나고 !
-// DOMContentLoaded : DOM 구조가 로드되어도 ! 실행 시점이 더 빠름
+// load : 모든 로드가 다 끝나고!
+// DOMContentLoaded : DOM 구조가 로드되어도! 실행 시점이 더 빠름!
 // window.addEventListener("DOMContentLoaded", function () {
-//   // 모든 로드가 다 끝나고 !
 //   const h1 = document.querySelector("h1");
 //   console.log(h1);
 // });
 function btn1() {
   console.log(document.head);
-  comsole.log(document.title);
+  console.log(document.title);
   console.log(document.body);
-  comsole.log(document.h1);
 
   // 동일한 태그가 여러개 존재할 수 있기 때문에 배열로 리턴
   const div = document.getElementsByTagName("div");
-  console.log(div[0]); //   <div>Hello , World</div>
-  console.log(div[1]); //   <div>Hello , JavaScript</div>
+  console.log(div);
+  console.log(div[0]); // <div>Hello, World!</div>
+  console.log(div[1]); // <div>Hello, JavaScript!</div>
 }
 
 function btn2() {
@@ -32,15 +31,16 @@ function btn3() {
 
 function btn4() {
   // 동일한 name 속성 값을 갖는 태그가 여러개 존재할 수 있기 때문에 배열로 리턴
-  const div = document.getElementsByName("testname");
+  const div = document.getElementsByName("testName");
   console.log(div);
+  console.log(document.testName);
+  console.log(document.frm);
 }
 
-// 이친구만 알고 있으면 된다 !!! 위에있는 걸 다 통합한게 이거다 querySelector 만 ㅎㅎㅎ !!!
 function btn5() {
-  const div = document.querySelector("#testId");
+  let div = document.querySelector("#testId");
   console.log(div);
-  document.querySelectorAll(".testClass");
+  div = document.querySelectorAll(".testClass");
   console.log(div);
   console.log(div[0]);
 }
@@ -48,14 +48,14 @@ function btn5() {
 function btn6() {
   const div = document.querySelectorAll(".testClass");
   div[0].textContent = "<span>안녕하세요</span>";
-  din[1].innerHTML = "<span>안녕하세요</span>";
+  div[1].innerHTML = "<span>안녕하세요</span>";
 }
 
 function btn7() {
   const div = document.querySelector("#testId");
   div.setAttribute("data-test", "테스트");
   div.setAttribute("data-test", "테스트2");
-  console.log(div.getAnimations("class"));
+  console.log(div.getAttribute("class"));
 }
 
 function btn8() {
@@ -64,7 +64,7 @@ function btn8() {
   div.style.backgroundColor = "yellow";
 }
 
-const div = document.querySelector("#testId2");
+const div2 = document.querySelector("#testId2");
 
 function btn9() {
   div2.classList.add("black");
@@ -74,6 +74,7 @@ function btn10() {
 }
 function btn11() {
   const check = div2.classList.contains("black");
+  console.log(check);
   if (check) {
     div2.classList.remove("black");
     div2.classList.add("white");
@@ -85,7 +86,6 @@ function btn11() {
 function btn12() {
   div2.classList.toggle("black");
 }
-
 function btn13() {
   // 자식 객체 생성
   const p = document.createElement("p");
